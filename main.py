7a041e67e29a68cde65c1103e9a5a5542aacbf3b
sunakk8 <112729438+sunakk8@users.noreply.github.com>
2024-09-14 18:58:06 +0000

    clean data

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ df = pd.read_csv("data.csv")
 drop_cols = [0,1,2,3,4,5,6,8,12,24,25,30]
 df.drop(df.columns[drop_cols],axis=1,inplace=True)
 
-# column_list = df.columns.to_list()
+column_list = df.columns.to_list()
 
-# for i in range(len(column_list)):
-#     print(i, ": ", column_list[i])
+for i in range(len(column_list)):
+    print(i, ": ", column_list[i])
+
+pd.set_option('display.max_columns', 500)
+
+df.to_csv('cleaned_data.csv', index=False)  
