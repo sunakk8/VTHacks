d20bfd0763083f62a2ad91ee87bd4e0c7f9a056c
sunakk8 <112729438+sunakk8@users.noreply.github.com>
2024-09-14 18:50:03 +0000

    clean data, update readme

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # VTHacks
 
-This project is a machine learning based application that predicts the type of social media used by an individual based on their profession.
+This project is a machine learning based application that predicts the profession of an individual based on their social media statistics.
--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import numpy as np
 import pandas as pd
 
+df = pd.read_csv("data.csv")
+
+#drop useless metrics
+drop_cols = [0,1,2,3,4,5,6,8,12,24,25,30]
+df.drop(df.columns[drop_cols],axis=1,inplace=True)
+
+# column_list = df.columns.to_list()
+
+# for i in range(len(column_list)):
+#     print(i, ": ", column_list[i])
